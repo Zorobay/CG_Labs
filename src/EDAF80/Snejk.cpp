63642c2431c7fe4e3eeb34ce870795679c3a3e45
Sebastian Hegardt <se.hegardt@gmail.com>
2018-10-15 10:28:11 +0200

    wow

---
+++ src/EDAF80/Snejk.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sebastian on 2018-10-15.
+//
+
+#include "Snejk.h"
---
+++ src/EDAF80/Snejk.h
@@ -0,0 +1,14 @@
+//
+// Created by sebastian on 2018-10-15.
+//
+
+#ifndef CG_LABS_SNEJK_H
+#define CG_LABS_SNEJK_H
+
+
+class Snejk {
+    friend class 
+};
+
+
+#endif //CG_LABS_SNEJK_H
