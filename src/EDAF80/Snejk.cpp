63d7f25605b7e1ab716455b3818330eea3948564
Sebastian Hegardt <se.hegardt@gmail.com>
2018-10-15 11:03:00 +0200

    Updated gitignore

--- src/EDAF80/Snejk.cpp
+++ src/EDAF80/Snejk.cpp
@@ -2,4 +2,14 @@
 // Created by sebastian on 2018-10-15.
 //
 
+#include <core/node.hpp>
+#include <core/helpers.hpp>
 #include "Snejk.h"
+
+
+Snejk::Snejk(const GLuint *const shader, const std::function<void(GLuint)> &set_uniforms, bonobo::mesh_data shape) {
+    auto head_node = Node();
+    head_node.set_program(shader, set_uniforms);
+    head_node.set_geometry(shape);
+}
+
--- src/EDAF80/Snejk.h
+++ src/EDAF80/Snejk.h
@@ -5,9 +5,14 @@
 #ifndef CG_LABS_SNEJK_H
 #define CG_LABS_SNEJK_H
 
+#include "core/Misc.h"
+#include "core/node.hpp"
+#include "core/ShaderProgramManager.hpp"
 
-class Snejk {
-    friend class 
+class Snejk: {;
+public:
+    Snejk(const GLuint * const shader, const std::function<void(GLuint)> &set_uniforms, bonobo::mesh_data shape);
+    void render();
 };
 
 
